65b546569d082ab447c05d1f2fb650ed56ba28ce
Costa <ancst@yandex.ru>
2022-02-28 10:55:58 +0300

    update heroku logs

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from "redux";
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import createSagaMiddleware from "redux-saga";
 import { sagaWatcher } from './sagas/saga';
 import { heroReducer } from './reducers/heroReducer';
 
 const saga = createSagaMiddleware();
-export const store = createStore(heroReducer, composeWithDevTools(applyMiddleware(saga)));
+export const store = createStore(heroReducer, applyMiddleware(saga));
 saga.run(sagaWatcher);
