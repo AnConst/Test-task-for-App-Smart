9106ab565bc3d87ca6635cafafd1ea622bf3e473
Costa <ancst@yandex.ru>
2022-02-28 11:01:04 +0300

    debug server

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
  const path = require('path');
  const port = process.env.PORT || 8080;
+ const favicon = require('express-favicon');
  
  // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
  const app = express();
+ app.use(favicon(__dirname + '/build/favicon.png')); 
  
  //здесь наше приложение отдаёт статику
  app.use(express.static(__dirname));
